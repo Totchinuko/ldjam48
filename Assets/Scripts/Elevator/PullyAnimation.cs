2b0f5ff610d2ad0ce6007567b0edce48f350eff4
yodaxy <82049516+yodaxy@users.noreply.github.com>
2021-04-26 17:21:58 +0200

    Level graphics, / Elevator / vfx

--- Assets/Scripts/Elevator/PullyAnimation.cs
+++ Assets/Scripts/Elevator/PullyAnimation.cs
@@ -8,11 +8,11 @@ public class PullyAnimation : MonoBehaviour
     [Header("Parameter Rotate")]
     [SerializeField] float _rotateTime = 0.5f;
     //[SerializeField] float _delay = 1f;
-    [SerializeField] private Vector3 _rotation = new Vector3(0, 0, 1);
+    [SerializeField] private Vector3 _rotation/* = new Vector3(0, 0, 1)*/;
 
     [Header("DoTween")]
     [SerializeField] RotateMode _rotateMode;
-    //[SerializeField] Ease _ease;
+    [SerializeField] Ease _ease;
 
     [Header("Target")]
     [SerializeField] ElevatorController _elevator;
@@ -22,6 +22,11 @@ public class PullyAnimation : MonoBehaviour
     private void Awake()
     {
         _transform = transform;
+        _sequence = DOTween.Sequence();
+        _sequence.Append(_transform.DORotate(_rotation, _rotateTime, _rotateMode).SetEase(_ease));
+        _sequence.SetAutoKill(false);
+        _sequence.SetLoops(-1);
+        _sequence.Pause();
     }
 
     private void Update()
@@ -29,21 +34,19 @@ public class PullyAnimation : MonoBehaviour
         //si l'ascenseur descend
         if (_elevator._isGoingDown)// à remplacer avec event
         {
-            ElevatorGoesDown();
+            _sequence.Play();
         }
-        //else
-        //{
-        //    ElevatorStopped();
-        //}
+        else
+        {
+            _sequence.Pause();
+        }
     }
 
-    private Tween ElevatorGoesDown()
+    private void ElevatorGoesDown()
     {
-        Sequence _sequence = DOTween.Sequence();
-        _sequence.Join(_transform.DORotate(_rotation, _rotateTime, _rotateMode)/*.SetDelay(_delay).SetEase(_ease).SetLoops(-1)*/);
-        return _sequence;
         //_transform.DORotate(_rotation, _rotateTime, _rotateMode).SetDelay(_delay).SetEase(_ease).SetLoops(-1);
         //Debug.Log("vers Satan, toute!");
+        
     }
 
     //private void ElevatorStopped()
@@ -56,6 +59,7 @@ public class PullyAnimation : MonoBehaviour
     //}
 
     private Transform _transform;
+    private Sequence _sequence;
 
     private bool StartPully { get => _startPully; set => _startPully = value; }
 }
